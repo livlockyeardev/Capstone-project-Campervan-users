bb69e00fb28ef2defd55351566f225c5c6c13713
livlockyeardev <livlockyear@gmail.com>
2026-02-16 09:04:26 +0000

    add edit/delete listing and listing detail page

--- listing/urls.py
+++ listing/urls.py
@@ -1,8 +1,11 @@
 from . import views
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
     path('', views.ListingList.as_view(), name='home'),
     path('create/', views.ListingCreate.as_view(), name='create-listing'),
     path('managelisting/', views.ManageListings.as_view(), name='manage-listings'),
+    path('edit/<slug:slug>/', views.ListingUpdate.as_view(), name='edit-listing'),
+    path('delete/<slug:slug>/', views.ListingDelete.as_view(), name='delete-listing'),
+    path('<slug:slug>/', views.ListingDetail.as_view(), name='listing-detail'),
 ]
--- listing/views.py
+++ listing/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from .models import Listing
 from .forms import ListingForm
@@ -12,6 +12,12 @@ class ListingList(generic.ListView):
     template_name = "listing/index.html"
 
 
+class ListingDetail(generic.DetailView):
+    model = Listing
+    template_name = "listing/listing_detail.html"
+    context_object_name = "listing"
+
+
 class ListingCreate(LoginRequiredMixin, generic.CreateView):
     model = Listing
     form_class = ListingForm
@@ -21,7 +27,7 @@ class ListingCreate(LoginRequiredMixin, generic.CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-    
+   
 
 class ManageListings(LoginRequiredMixin, generic.ListView):
     model = Listing
@@ -30,3 +36,24 @@ class ManageListings(LoginRequiredMixin, generic.ListView):
 
     def get_queryset(self):
         return Listing.objects.filter(author=self.request.user).order_by("-created_on")
+
+
+class ListingUpdate(LoginRequiredMixin, UserPassesTestMixin, generic.UpdateView):
+    model = Listing
+    form_class = ListingForm
+    template_name = "listing/edit_listing.html"
+    success_url = reverse_lazy("manage-listings")
+
+    def test_func(self):
+        listing = self.get_object()
+        return self.request.user == listing.author
+
+
+class ListingDelete(LoginRequiredMixin, UserPassesTestMixin, generic.DeleteView):
+    model = Listing
+    template_name = "listing/delete_listing.html"
+    success_url = reverse_lazy("manage-listings")
+
+    def test_func(self):
+        listing = self.get_object()
+        return self.request.user == listing.author
