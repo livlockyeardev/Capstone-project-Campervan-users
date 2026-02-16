32379ae772149be11c811f1735041c4f502e32bb
livlockyeardev <livlockyear@gmail.com>
2026-02-16 07:28:40 +0000

    add styling to create listing form

--- listing/forms.py
+++ listing/forms.py
@@ -3,6 +3,7 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 from .models import Listing
 
+
 class ListingForm(forms.ModelForm):
     class Meta:
         model = Listing
@@ -17,6 +18,18 @@ class ListingForm(forms.ModelForm):
             "max_nights",
             "price_per_night",
         )
+
+        labels = {
+            "title": "Title (Maximum Five Words)",
+            "featured_image": "Display Image",
+            "description": "Description (Maximum 200 Words)",
+            "location": "Location (Maximum 20 Characters)",
+            "toilet_available": "Facilities Available?",
+            "noise_level": "Noise Level",
+            "min_nights": "Minimum Nights",
+            "max_nights": "Maximum Nights",
+            "price_per_night": "Price Per Night (We suggest keeping a price under £5 if no facilities are available and under £10 if there are.)",
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
--- listing/models.py
+++ listing/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.utils.text import slugify
 
 
 NOISE_LEVELS = ((0, "Quiet"), (1, "Moderate"), (2, "Loud"))
@@ -25,6 +26,11 @@ class Listing(models.Model):
 
     class Meta:
         ordering = ["-created_on"]
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f"{self.title}"
